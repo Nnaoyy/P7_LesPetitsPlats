b67f36d8511907557353ea09461897db47bc1ccf
Nnaoyy <yoann.jamain@hotmail.fr>
2023-08-11 10:26:16 +0200

    ajout du msg si pas de resultat a la recherche

--- scripts/utils/display.js
+++ scripts/utils/display.js
@@ -1,10 +1,21 @@
 export function displayData(recipes) {
+    const searchInput = document.getElementById('searchInput');
     const recipesSection = document.getElementById('gallery-container');
     recipesSection.innerHTML = '';
     
-    recipes.map(recipe =>  new Recipe(recipe))
-    .forEach((recipe) => {           
-        const Template = new RecipeCard(recipe);
-        recipesSection.appendChild(Template.getRecipeCardDOM());
-    });
+    if (recipes.length === 0) {
+		const notFound = document.createElement('p');
+		notFound.className = 'notFound';
+
+		notFound.innerText =
+			"Aucune recette ne contient '" + searchInput.value + "' vous pouvez chercher 'tarte aux pommes', 'poisson', etc.";
+
+        recipesSection.appendChild(notFound);
+	} else {
+        recipes.map(recipe =>  new Recipe(recipe))
+        .forEach((recipe) => {           
+            const Template = new RecipeCard(recipe);
+            recipesSection.appendChild(Template.getRecipeCardDOM());
+        });
+    }
 }
